1ac9a3bd7873e5cdfeb4236bc1f839b61494d03f
ammarhussien <ammar.hussienmo97@gmail.com>
2023-03-20 14:06:09 +0200

    start app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import './providers/great_places.dart';
+import 'package:provider/provider.dart';
+
+import 'screens/places_list_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,18 +11,23 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = ThemeData();
-    return MaterialApp(
-      title: 'Great Places',
-    theme: theme.copyWith(
-    colorScheme: theme.colorScheme.copyWith(primary: Colors.indigo, secondary: Colors.amber,),
+    return ChangeNotifierProvider(
+      create: (context) => GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: theme.copyWith(
+          colorScheme: theme.colorScheme.copyWith(
+            primary: Colors.indigo,
+            secondary: Colors.amber,
+          ),
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const PlacesListScreen(),
       ),
-      home: const ;
     );
   }
 }
-
